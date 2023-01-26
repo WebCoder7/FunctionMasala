0ea2d6fe41f1813ef2431747877313660106d130
WebCoder7 <gulomovgulomjon296@gmail.com>
2023-01-26 17:04:32 +0200

    lol

---
+++ index.js
@@ -0,0 +1,34 @@
+// 1 - masal
+// function funct (num) {
+// 	if(num<=6){
+//     return true
+//   }else{
+//     return false
+//   }
+// }
+// funct () 
+
+
+
+
+// 6-masala
+// func ()
+// let funct = prompt ("raqam kiriting")
+// function func(num) {
+// switch (num) {
+//      case "6":
+//        alert ("Sizni yoshiz 18 da")
+//        break
+//      default:
+//        alert("Sizni raqamiz nomalum")
+// }
+// }
+// console.log(func);
+
+
+
+
+//   3-masala
+// for ( let i = 1; i <=6; i++){
+//     console.log(i);
+// }
